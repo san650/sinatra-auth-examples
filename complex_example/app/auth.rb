1eda94ce59cf9d7704f909aaf883fe77a5da8157
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2014-07-04 16:07:10 -0300

    Complex example: add facebook auth

--- complex_example/app/auth.rb
+++ complex_example/app/auth.rb
@@ -1,26 +1,28 @@
 require "omniauth"
 require "omniauth/strategies/github"
+require "omniauth/strategies/facebook"
 
 class Auth < Protected
 
   use OmniAuth::Builder do
     provider :developer, callback_path: "/login/auth/developer/callback"
     provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
+    provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"]
   end
 
   get "/" do
     erb :login
   end
 
-  get "/auth/github/callback" do
+  get "/auth/:provider/callback" do |provider|
     self.current_user = User.new(
       request.env["omniauth.auth"]["uid"],
       request.env["omniauth.auth"]["info"]["name"],
       request.env["omniauth.auth"]["info"]["email"],
-      :github
+      provider.to_sym
     )
 
-    "You're authenticated with github! Go to the <a href=\"/admin\">admin</a>"
+    "You're authenticated with #{provider}! Go to the <a href=\"/admin\">admin</a>"
   end
 
   post "/auth/developer/callback" do
